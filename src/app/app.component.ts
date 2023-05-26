0e150e4b036251cc5548367242737b9d59299894
john clampet <jsclampet@gmail.com>
2023-05-26 13:14:37 -0500

    installed and import bulma(css) and faker(js, filler data)...used compare function for class styling per letter, with ternary operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'optionalApp';
+  randomText: string = faker.lorem.sentence();
+  userInput: string = '';
+
+  onUserInputChange(value: string) {
+    this.userInput = value;
+  }
+
+  compare( loremLetter: string, inputLetter: string) {
+    if (!inputLetter) {
+      return "pending";
+    }
+    return inputLetter === loremLetter ? "correct" : "incorrect";
+  }
 }
